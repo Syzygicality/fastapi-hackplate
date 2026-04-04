6d59e14349b9bc65458e1c2fca2f9c563f9a9802
Edison Wang <ew040607@gmail.com>
2026-04-04 19:39:29 -0400

    fix: proper type hinting on get session

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,7 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.config.config import HackplateRequest
 
 
-def get_session(request: HackplateRequest):
+def get_session(request: HackplateRequest) -> AsyncSession:
     return request.app.state.config.db.get_session()
